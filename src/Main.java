ee4abaf3ebd2e677c5b8abb3c03df126cd254bd4
TomWulf <Tom.Wulf@uc.edu>
2023-04-06 13:52:24 -0400

    Works, not all tags inplemted yet

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 public class Main {
+
+    final static String nl = "\n";
+    final static String tab = "\t";
+    final static String quote = "\"";
+
     public static void main(String[] args)
     {
         StringBuilder HTMLPage = new StringBuilder();
-
-        String nl = "\n";
-        String tab = "\t";
 
         HTMLPage.append(createHTMLPageHead("Test", "test.css"));
         HTMLPage.append(startHTMLElem("body"));
@@ -15,7 +19,11 @@ public class Main {
         HTMLPage.append(endHTMLElem("header"));
 
         HTMLPage.append(startHTMLElem("main"));
-        HTMLPage.append("<!-- Main CONTENT -->" + nl);
+            Map<String, String> pAttr = new HashMap<>();
+            pAttr.put("ID", "TopPara");
+        HTMLPage.append(HTMLElem("p",pAttr,"Test paragraph with ID Attribute" ));
+        HTMLPage.append(ol(Arrays.asList(new String[]{"tom", "dick", "harry"}) ));
+        HTMLPage.append(ul(Arrays.asList(new String[]{"tom", "dick", "harry"}) ));
         HTMLPage.append(endHTMLElem("main"));
 
         HTMLPage.append(startHTMLElem("footer"));
@@ -30,8 +38,7 @@ public class Main {
 
     private static String createHTMLPageHead(String title, String CSSLink )
     {
-        String nl = "\n";
-        String tab = "\t";
+
         StringBuilder markUp = new StringBuilder("<!DOCTYPE html>" + nl);
         markUp.append("<html>" + nl);
         markUp.append("<head>" + nl);
@@ -48,9 +55,27 @@ public class Main {
 
     private static String HTMLElem(String tag, String content)
     {
-        String nl = "\n";
-        String tab = "\t";
+
         StringBuilder markUp = new StringBuilder("<" + tag + ">" + nl);
+        markUp.append(content);
+        markUp.append("</" + tag + ">" + nl);
+
+        return markUp.toString();
+    }
+
+    private static String HTMLElem(String tag, Map<String, String> attributes, String content)
+    {
+
+        StringBuilder markUp = new StringBuilder("<" + tag);
+          int attTotal = attributes.size();
+          int attCount = 0;
+          for(String k : attributes.keySet()) {
+              attCount++;
+              markUp.append(" " + k.trim() + ":" + quote + attributes.get(k).trim() + quote);
+              if(attCount != attTotal)
+                  markUp.append(";");
+          }
+        markUp.append(">");
         markUp.append(content);
         markUp.append("</" + tag + ">" + nl);
 
@@ -59,21 +84,58 @@ public class Main {
 
     private static String startHTMLElem(String tag)
     {
-        String nl = "\n";
-        String tab = "\t";
+
         StringBuilder markUp = new StringBuilder("<" + tag + ">" + nl);
+
+        return markUp.toString();
+    }
+
+    private static String startHTMLElem(String tag,Map<String, String> attributes)
+    {
+
+        StringBuilder markUp = new StringBuilder("<" + tag);
+        int attTotal = attributes.size();
+        int attCount = 0;
+        for(String k : attributes.keySet()) {
+            attCount++;
+            markUp.append(" " + k.trim() + ":" + attributes.get(k).trim());
+            if(attCount != attTotal)
+                markUp.append(";");
+        }
+        markUp.append(">");
 
         return markUp.toString();
     }
 
     private static String endHTMLElem(String tag)
     {
-        String nl = "\n";
-        String tab = "\t";
+
         StringBuilder markUp = new StringBuilder("</" + tag + ">" + nl);
 
         return markUp.toString();
     }
 
+    private static String ol(List<String> items)
+    {
+        StringBuilder markUp = new StringBuilder("<ol>" + nl);
 
+        for(String i:items)
+            markUp.append(tab + "<li>" + i + "</li>" + nl);
+
+        markUp.append("</ol>" + nl);
+
+        return markUp.toString();
+    }
+
+    private static String ul(List<String> items)
+    {
+        StringBuilder markUp = new StringBuilder("<ul>" + nl);
+
+        for(String i:items)
+            markUp.append(tab + "<li>" + i + "</li>" + nl);
+
+        markUp.append("</ul>" + nl);
+
+        return markUp.toString();
+    }
 }
