c5c16e26414902e31fecf1a6c115dc9428f0eb2a
TomWulf <Tom.Wulf@uc.edu>
2023-04-13 13:55:08 -0400

    Works, implemented the start of the CSS System

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class Main {
 
     final static String nl = "\n";
     final static String tab = "\t";
     final static String quote = "\"";
-
+    final static Map<String, Map<String, String>> CSSData = new TreeMap<>();
     public static void main(String[] args)
     {
+
+
         StringBuilder HTMLPage = new StringBuilder();
 
         HTMLPage.append(createHTMLPageHead("Test", "test.css"));
@@ -15,13 +22,14 @@ public class Main {
         HTMLPage.append(startHTMLElem("header"));
         HTMLPage.append(startHTMLElem("nav"));
            HTMLPage.append("<!-- NAV CONTENT -->" + nl);
+ //          Map<String, String> n1Attr = new HashMap<>();
+ //          n1Attr.put("href", "http://homepages.uc.edu/~wulft");
+           HTMLPage.append(HTMLElem("a", "href", "http://homepages.uc.edu/~wulft", "Tom's UC WebSite"));
         HTMLPage.append(endHTMLElem("nav"));
         HTMLPage.append(endHTMLElem("header"));
 
         HTMLPage.append(startHTMLElem("main"));
-            Map<String, String> pAttr = new HashMap<>();
-            pAttr.put("ID", "TopPara");
-        HTMLPage.append(HTMLElem("p",pAttr,"Test paragraph with ID Attribute" ));
+        HTMLPage.append(HTMLElem("p","ID", "TopPara","Test paragraph with ID Attribute" ));
         HTMLPage.append(ol(Arrays.asList(new String[]{"tom", "dick", "harry"}) ));
         HTMLPage.append(ul(Arrays.asList(new String[]{"tom", "dick", "harry"}) ));
         HTMLPage.append(endHTMLElem("main"));
@@ -34,13 +42,33 @@ public class Main {
 
         System.out.println(HTMLPage.toString());
 
+
+        try {
+            savePageFile("Test.html", HTMLPage.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // CSS test
+        addCSSProperty("body","background-color", "yellow");
+        addCSSProperty("body","color", "blue");
+        addCSSProperty("body","margin", "5%");
+        addCSSProperty("header","background-color", "blue");
+        addCSSProperty("header","color", "white");
+
+        try {
+            saveCSSFile("test.css");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static String createHTMLPageHead(String title, String CSSLink )
     {
 
         StringBuilder markUp = new StringBuilder("<!DOCTYPE html>" + nl);
-        markUp.append("<html>" + nl);
+        markUp.append("<html lang="+ quote + "en" + quote + ">" + nl);
         markUp.append("<head>" + nl);
         markUp.append(tab + "<title>" + title + "</title>" + nl);
         if(CSSLink != null)
@@ -63,6 +91,18 @@ public class Main {
         return markUp.toString();
     }
 
+    private static String HTMLElem(String tag, String key, String value, String content)
+    {
+
+        StringBuilder markUp = new StringBuilder("<" + tag + " " + key + "=" + quote + value + quote + ">" + nl);
+        markUp.append(content);
+        markUp.append("</" + tag + ">" + nl);
+
+        return markUp.toString();
+    }
+
+
+
     private static String HTMLElem(String tag, Map<String, String> attributes, String content)
     {
 
@@ -71,7 +111,7 @@ public class Main {
           int attCount = 0;
           for(String k : attributes.keySet()) {
               attCount++;
-              markUp.append(" " + k.trim() + ":" + quote + attributes.get(k).trim() + quote);
+              markUp.append(" " + k.trim() + "=" + quote + attributes.get(k).trim() + quote);
               if(attCount != attTotal)
                   markUp.append(";");
           }
@@ -90,6 +130,18 @@ public class Main {
         return markUp.toString();
     }
 
+    private static String startHTMLElem(String tag, String key, String value)
+    {
+
+        StringBuilder markUp = new StringBuilder("<" + tag);
+
+        markUp.append(" " + key + "=" + quote + value + quote);
+
+        markUp.append(">");
+
+        return markUp.toString();
+    }
+
     private static String startHTMLElem(String tag,Map<String, String> attributes)
     {
 
@@ -98,7 +150,7 @@ public class Main {
         int attCount = 0;
         for(String k : attributes.keySet()) {
             attCount++;
-            markUp.append(" " + k.trim() + ":" + attributes.get(k).trim());
+            markUp.append(" " + k.trim() + "=" + attributes.get(k).trim());
             if(attCount != attTotal)
                 markUp.append(";");
         }
@@ -138,4 +190,106 @@ public class Main {
 
         return markUp.toString();
     }
+
+    private static String quoteString(String needsQuotes)
+    {
+        return quote + needsQuotes + quote;
+    }
+
+    public static void savePageFile(String fileName, String HTMLText) throws IOException
+    {
+        // Typical java pattern of inherited classes
+        // we wrap a BufferedWriter around a lower level BufferedOutputStream
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\" + fileName);
+
+        OutputStream out =
+                new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+        BufferedWriter writer =
+                new BufferedWriter(new OutputStreamWriter(out));
+
+        // Finally can write the file LOL!
+
+
+            writer.write(HTMLText, 0, HTMLText.length());  // stupid syntax for write rec
+            // 0 is where to start (1st char) the write
+            // rec. length() is how many chars to write (all)
+            writer.newLine();  // adds the new line
+
+
+        writer.close(); // must close the file to seal it and flush buffer
+        System.out.println("Data file written!");
+    }
+
+    // CSS Functions
+    public static void addCSSProperty(String selector, String prop, String value)
+    {
+        // see if this selector is already in the map
+        // if it is: add another prop value pair to its map
+        // if it is not: create a new Map entry for this selector and add the property to it
+
+        if(CSSData.containsKey(selector))
+        {
+            CSSData.get(selector).put(prop, value);
+        }
+        else
+        {
+            // have to create new entry for this selector
+            Map<String,String> property = new TreeMap<>();
+            property.put(prop, value);
+            CSSData.put(selector, property);
+        }
+
+        System.out.println("Debug data dump: " + CSSData);
+    }
+
+    public static void saveCSSFile(String fileName) throws IOException
+    {
+        StringBuilder CSSOutput = new StringBuilder();
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\" + fileName);
+
+        OutputStream out =
+                new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+        BufferedWriter writer =
+                new BufferedWriter(new OutputStreamWriter(out));
+
+        // Finally can write the file LOL!
+
+        // go through the CSS data map
+        // for each key create a selector
+        // generate a rule body for the selector including each prop value pair
+
+
+        for(String sel : CSSData.keySet())
+        {
+            CSSOutput.append(sel + " {" + nl);
+            Map<String,String> props = CSSData.get(sel);
+            for(String key: props.keySet())
+            {
+                CSSOutput.append(tab + key + ":" + props.get(key) + ";" + nl);
+            }
+            CSSOutput.append("}" + nl);
+
+        }
+
+        System.out.println("Data dump CSS map:" + nl);
+        System.out.println(CSSData);
+
+        System.out.println("data dump of the file text:");
+        System.out.println(nl + nl + CSSOutput);
+
+
+        writer.write(CSSOutput.toString(), 0, CSSOutput.toString().length());  // stupid syntax for write rec
+        // 0 is where to start (1st char) the write
+        // rec. length() is how many chars to write (all)
+        writer.newLine();  // adds the new line
+
+
+        writer.close(); // must close the file to seal it and flush buffer
+        System.out.println("Data file written!");
+    }
+
 }
